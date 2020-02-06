55f14032cf199932039d86a676c1cd7434812c43
jpbacher <bacherjp@gmail.com>
2020-02-06 06:04:33 -0500

    predict method

--- models/tree.py
+++ models/tree.py
@@ -33,34 +33,65 @@ class Tree:
                  min_samples=1,
                  gain_thresh=0.1,
                  max_points=None):
-        if max_points is None:
-            max_points = X.shape[0]
-        self.leaf = False
-        self.gain = -1.0 * np.inf
-        for feat in X.columns:
-            if X[feat].dtype == 'O':
-                # get all the points in the feature
-                mask = X[feat]
-                gain = info_gain(y, mask, imp_func)
-                if gain > self.gain:
-                    self.gain = gain
-                    self.feat = feat
-                    best_mask = mask
-            else:
-                # numerical features
-                thresholds = np.linspace(X[feat].min(), X[feat].max(), num=num)
-                for i, thresh in enumerate(thresholds):
-                    mask = X[feat] > thresh
+        if height > 0 and X.shape[0] > min_samples:
+            if max_points is None:
+                max_points = X.shape[0]
+            self.leaf = False
+            self.gain = -1.0 * np.inf
+            for feat in X.columns:
+                if X[feat].dtype == 'O':
+                    # get all the points in the feature
+                    mask = X[feat]
                     gain = info_gain(y, mask, imp_func)
                     if gain > self.gain:
                         self.gain = gain
-                        self.thresh = thresh
                         self.feat = feat
                         best_mask = mask
+                else:
+                    # numerical features
+                    thresholds = np.linspace(X[feat].min(), X[feat].max(), num=num)
+                    for i, thresh in enumerate(thresholds):
+                        mask = X[feat] > thresh
+                        gain = info_gain(y, mask, imp_func)
+                        if gain > self.gain:
+                            self.gain = gain
+                            self.thresh = thresh
+                            self.feat = feat
+                            best_mask = mask
             self.gain_scaled = self.gain * X_shape[0]
             self.gain_scaled_norm = self.gain_scaled / max_points
             if self.gain < gain_thresh:
                 self.leaf = True
                 self.val = agg_func(y)
                 return
-                 
+            self.children = []
+            self.cats = np.unique(best_mask)
+            for i in self.cats:
+                self.children.append(Tree(
+                    X = X[best_mask == i],
+                    y = y[best_mask == i],
+                    height = height-1,
+                    imp_func = imp_func,
+                    agg_func = agg_func,
+                    num=num,
+                    min_samples=min_samples,
+                    gain_thresh=gain_thresh,
+                    max_points=max_points
+                ))
+        else:
+            self.leaf = True
+            self.val = agg_func(y)
+
+    def predict(self, X):
+        out = np.ones((X.shape[0], 1))
+        if self.leaf:
+            return out * self.val
+        else:
+            if X[self.feat].dtype != 'O':
+                mask = X[self.feat] > self.thresh
+            else:
+                mask = X[self.feat]
+            for i, c in self.enumerate(self.cats):
+                out[c == mask] = self.children[i].predict(X[c == mask])
+            return out
+               
